0b7bc288d9e37a46ffc626afc2b5e91ff69ad543
Wei Tung Chen <weitungchen1998@gmail.com>
2023-11-15 22:38:42 -0800

    added eslint

---
+++ src/layouts/BaseLayout/index.tsx
@@ -0,0 +1,30 @@
+---
+import Foot from "../../components/Foot.astro";
+import Head from "../../components/Head.astro";
+import Menu from "../../components/Menu/index.astro";
+import { type IIncludePlugins } from "../../types";
+import "../../assets/styles/typography.scss";
+import "./index.scss";
+
+export type Props = {
+  title: string;
+  includePlugins?: IIncludePlugins;
+  includeMenu?: boolean;
+};
+
+const { title, includeMenu = false, includePlugins } = Astro.props;
+---
+
+<!doctype html>
+<html lang="en">
+  <Head title={title} includePlugins={includePlugins} />
+
+  <body>
+    <div id="background"></div>
+
+    {includeMenu && <Menu />}
+    <slot />
+
+    <Foot includePlugins={includePlugins} />
+  </body>
+</html>
---
+++ src/layouts/HomeLayout/index.tsx
@@ -0,0 +1,14 @@
+---
+import BaseLayout, {
+  type Props as BaseLayoutProps,
+} from "../BaseLayout/index.astro";
+import "./index.scss";
+
+type Props = Omit<BaseLayoutProps, "includeMenu">;
+
+const props = Astro.props;
+---
+
+<BaseLayout includeMenu {...props}>
+  <slot />
+</BaseLayout>
